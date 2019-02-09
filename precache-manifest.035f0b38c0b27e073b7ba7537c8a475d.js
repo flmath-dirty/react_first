060669d59cd8a2d8dc83d42e0b1cdbb9b8527064
Mathias Green <flmathematic@gmail.com>
2019-02-09 16:00:41 +0100

    added build

---
+++ precache-manifest.035f0b38c0b27e073b7ba7537c8a475d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react_first/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "277b1d97c5f94e3e34e7",
+    "url": "/react_first/static/js/runtime~main.277b1d97.js"
+  },
+  {
+    "revision": "df840fce3c9f3fcb1776",
+    "url": "/react_first/static/js/main.df840fce.chunk.js"
+  },
+  {
+    "revision": "13eeb2032981bf3107f9",
+    "url": "/react_first/static/js/1.13eeb203.chunk.js"
+  },
+  {
+    "revision": "df840fce3c9f3fcb1776",
+    "url": "/react_first/static/css/main.e5fc0507.chunk.css"
+  },
+  {
+    "revision": "a17ab8e3fa45b912559ad51314edaf8c",
+    "url": "/react_first/index.html"
+  }
+];
